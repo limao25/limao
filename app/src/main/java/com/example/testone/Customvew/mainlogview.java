c8377a836d7bea8a2c43091556fb2075f815c5cc
amao <3122620995@qq.com>
2020-12-08 18:21:51 +0800

    小控件

--- app/src/main/java/com/example/testone/Customvew/mainlogview.java
+++ app/src/main/java/com/example/testone/Customvew/mainlogview.java
@@ -1,5 +1,23 @@
 package com.example.testone.Customvew;
 
-public class mainlogview {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class mainlogview extends View {
+
+    public mainlogview(Context context) {
+        super(context);
+    }
+
+    public mainlogview(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public mainlogview(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
 }
--- app/src/main/java/com/example/testone/MainActivity.java
+++ app/src/main/java/com/example/testone/MainActivity.java
@@ -3,13 +3,37 @@ package com.example.testone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.ypk.library.interfac.OnTabClickListener;
+import com.ypk.library.view.YPKTabLayoutView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> tabtextlist = new ArrayList<>();
+    private YPKTabLayoutView mYPKTabLayoutView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mYPKTabLayoutView = (YPKTabLayoutView) findViewById(R.id.mYPKTabLayoutView);
+//
+//        tabtextlist.add("推荐学习");
+//        tabtextlist.add("企业学院");
+//        tabtextlist.add("我的关注");
+//
+//        mYPKTabLayoutView.setTabTextList(tabtextlist);
+//
+//        mYPKTabLayoutView.setOnTabClickListener(new OnTabClickListener() {
+//            @Override
+//            public void tabSelectedListener(int i) { Toast.makeText(MainActivity.this, tabtextlist.get(i), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "" + tabtextlist.get(i), Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
     }
 
